22472af74a8f894be78ed4ee56cdec7c89bcd826
Vlad <vlad@example.com>
2026-05-01 01:02:14 +0300

    platform IO 2nd

---
+++ ESP_Panel_Conf.h
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ESP_PANEL_CONF_H
+#define ESP_PANEL_CONF_H
+
+// *INDENT-OFF*
+/* Set to 0 if use a custom board */
+#define ESP_PANEL_USE_SUPPORTED_BOARD   (0)
+
+/* For custom board, all the configurations should be done in the board_config file.
+   This file just sets the flag to use custom board. */
+
+// *INDENT-ON*
+#endif // ESP_PANEL_CONF_H
